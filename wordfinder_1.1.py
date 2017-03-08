e09643c78a95469989dd9df70b1df54bc47b220c
nhx26 <muhammad.anas.gopee@gmail.com>
2017-03-08 21:03:02 +0400

    Add files via upload

---
+++ wordfinder_1.1.py
@@ -0,0 +1,27 @@
+#A.Gopee
+#word_finder_1.1
+
+#Use:
+#Input words and stop input with the word "end"
+#note: words not on merriam-webster will not load a webpage
+
+#v1.1: added delay to display web pages in correct order
+
+import webbrowser
+import time
+words = []
+count = 1
+while True:
+    word = raw_input("Input word " + str(count) + ": ")
+    if word <> "end":
+        
+        words.append(str(word))
+        count = count + 1
+    else:
+        break
+for word in words:
+    
+    url = "https://www.merriam-webster.com/dictionary/" + word
+    webbrowser.open_new_tab(url)
+    time.sleep(0.1)
+    
